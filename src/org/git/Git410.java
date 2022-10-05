f646ecdd585116a5cd5bd50e21d15c13f1765aed
githupavi <pavisara0610@gmail.com>
2022-10-05 07:01:30 +0530

    cloneproject

---
+++ src/org/git/Git410.java
@@ -0,0 +1,45 @@
+package org.git;
+
+
+
+public class Git410 {
+	
+	private void empId() {
+		System.out.println("Employee id is 96066");
+
+		}
+		private void empName() {
+		System.out.println("Employee Name is S.PAVIHRA");
+
+		}
+		private void empDob() {
+			System.out.println("Employee DOB is 10-6-1998");
+
+		}
+		private void empPhone() {
+		 	System.out.println("Employee Phone no is 9786092819");
+
+		}
+		private void empEmail() {
+		 	System.out.println("Employee E-MAIL is pavisara0610@gmail.com");
+
+		}
+		private void empAddress() {
+		System.out.println("Employee Address is no.12,abc road,xyz dt ");
+
+		}
+		public static void main(String[] args) {
+			Git410 employeeDetails = new Git410();
+			employeeDetails.empId();
+			employeeDetails.empName();
+			employeeDetails.empDob();
+			employeeDetails.empPhone();
+			employeeDetails.empEmail();
+			employeeDetails.empAddress();
+			
+			
+		}
+		
+
+
+}
